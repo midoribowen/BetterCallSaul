5974b2f8a6d820e4004e0d3b1909ecbb4c37b0af
Midori Bowen <midori.p.bowen@gmail.com>
2016-06-22 15:33:36 -0700

    Add BusinessList UI Fragment with shared preferences.

--- app/src/main/java/com/mpbowen/bettercallsaul/businessList/BusinessListActivity.java
+++ app/src/main/java/com/mpbowen/bettercallsaul/businessList/BusinessListActivity.java
@@ -29,83 +29,10 @@ import retrofit2.Response;
 
 public class BusinessListActivity extends AppCompatActivity {
 
-    public static final String TAG = BusinessListActivity.class.getSimpleName();
-
-    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
-
-    private BusinessListAdapter mAdapter;
-    public ArrayList<Business> mBusinesses = new ArrayList<>();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_business_list);
-        ButterKnife.bind(this);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_search, menu);
-        ButterKnife.bind(this);
-
-        MenuItem menuItem = menu.findItem(R.id.action_search);
-        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                getBusinesses(query);
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                return false;
-            }
-        });
-
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void getBusinesses(String location) {
-        String term = "legal services";
-        YelpAPIFactory apiFactory = new YelpAPIFactory(Constants.YELP_CONSUMER_KEY, Constants.YELP_CONSUMER_SECRET, Constants.YELP_TOKEN, Constants.YELP_TOKEN_SECRET);
-        YelpAPI yelpAPI = apiFactory.createAPI();
-        Call<SearchResponse> call = yelpAPI.search(term, location);
-        Callback<SearchResponse> callback = new Callback<SearchResponse>() {
-            @Override
-            public void onResponse(Call<SearchResponse> call, Response<SearchResponse> response) {
-                SearchResponse searchResponse = response.body();
-                try {
-                    mBusinesses = new ArrayList(searchResponse.getBusinesses());
-                } catch (NullPointerException exception) {
-                    exception.printStackTrace();
-                }
-
-                BusinessListActivity.this.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        mAdapter = new BusinessListAdapter(getApplicationContext(), mBusinesses);
-                        mRecyclerView.setAdapter(mAdapter);
-                        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(BusinessListActivity.this);
-                        mRecyclerView.setLayoutManager(layoutManager);
-                        mRecyclerView.setHasFixedSize(true);
-                    }
-                });
-
-            }
-
-            @Override
-            public void onFailure(Call<SearchResponse> call, Throwable t) {
-                Log.d(TAG, t.toString());
-            }
-        };
-        call.enqueue(callback);
-    }
 }
---
+++ app/src/main/java/com/mpbowen/bettercallsaul/businessList/BusinessListFragment.java
@@ -0,0 +1,144 @@
+package com.mpbowen.bettercallsaul.businessList;
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mpbowen.bettercallsaul.Constants;
+import com.mpbowen.bettercallsaul.R;
+import com.mpbowen.bettercallsaul.adapters.BusinessListAdapter;
+import com.mpbowen.bettercallsaul.models.Business;
+import com.mpbowen.bettercallsaul.models.SearchResponse;
+import com.mpbowen.bettercallsaul.services.YelpAPI;
+import com.mpbowen.bettercallsaul.services.YelpAPIFactory;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+
+public class BusinessListFragment extends Fragment {
+
+    public static final String TAG = BusinessListFragment.class.getSimpleName();
+
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+    private String mRecentAddress;
+
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+    private BusinessListAdapter mAdapter;
+    public ArrayList<Business> mBusinesses = new ArrayList<>();
+
+    public BusinessListFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_business_list, container, false);
+        ButterKnife.bind(this, view);
+
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+        mRecentAddress = mSharedPreferences.getString(Constants.PREFERENCES_LOCATION_KEY, null);
+        mEditor = mSharedPreferences.edit();
+        if (mRecentAddress != null) {
+            getBusinesses(mRecentAddress);
+        }
+
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_search, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                addToSharedPreferences(query);
+                getBusinesses(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+    }
+
+        @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void addToSharedPreferences(String location) {
+        mEditor.putString(Constants.PREFERENCES_LOCATION_KEY, location).apply();
+    }
+
+        private void getBusinesses(String location) {
+        String term = "legal services";
+        YelpAPIFactory apiFactory = new YelpAPIFactory(Constants.YELP_CONSUMER_KEY, Constants.YELP_CONSUMER_SECRET, Constants.YELP_TOKEN, Constants.YELP_TOKEN_SECRET);
+        YelpAPI yelpAPI = apiFactory.createAPI();
+        Call<SearchResponse> call = yelpAPI.search(term, location);
+        Callback<SearchResponse> callback = new Callback<SearchResponse>() {
+            @Override
+            public void onResponse(Call<SearchResponse> call, Response<SearchResponse> response) {
+                SearchResponse searchResponse = response.body();
+                try {
+                    mBusinesses = new ArrayList(searchResponse.getBusinesses());
+                } catch (NullPointerException exception) {
+                    exception.printStackTrace();
+                }
+
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mAdapter = new BusinessListAdapter(getContext(), mBusinesses);
+                        mRecyclerView.setAdapter(mAdapter);
+                        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+                        mRecyclerView.setLayoutManager(layoutManager);
+                        mRecyclerView.setHasFixedSize(true);
+                    }
+                });
+
+            }
+
+            @Override
+            public void onFailure(Call<SearchResponse> call, Throwable t) {
+                Log.d(TAG, t.toString());
+            }
+        };
+        call.enqueue(callback);
+    }
+
+}
