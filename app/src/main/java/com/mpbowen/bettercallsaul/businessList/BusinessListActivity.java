5e6c296ac6c80dbce9b6c62a9f6b575630128241
Midori Bowen <midori.p.bowen@gmail.com>
2016-06-23 13:10:05 -0700

    Refactor fragment initiation.

--- app/src/main/java/com/mpbowen/bettercallsaul/businessList/BusinessListActivity.java
+++ app/src/main/java/com/mpbowen/bettercallsaul/businessList/BusinessListActivity.java
@@ -1,31 +1,12 @@
 package com.mpbowen.bettercallsaul.businessList;
 
 import android.os.Bundle;
-import android.support.v4.view.MenuItemCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.SearchView;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 
-import com.mpbowen.bettercallsaul.Constants;
 import com.mpbowen.bettercallsaul.R;
-import com.mpbowen.bettercallsaul.adapters.BusinessListAdapter;
-import com.mpbowen.bettercallsaul.models.Business;
-import com.mpbowen.bettercallsaul.models.SearchResponse;
-import com.mpbowen.bettercallsaul.services.YelpAPI;
-import com.mpbowen.bettercallsaul.services.YelpAPIFactory;
-
-import java.util.ArrayList;
-
-import butterknife.Bind;
-import butterknife.ButterKnife;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class BusinessListActivity extends AppCompatActivity {
 
@@ -33,6 +14,17 @@ public class BusinessListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_business_list);
+
+        // Set up Collapsing Toolbar
+
+        initFragment(BusinessListFragment.newInstance());
+    }
+
+    private void initFragment(Fragment businessListFragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.contentFrame, businessListFragment);
+        fragmentTransaction.commit();
     }
 
 }
